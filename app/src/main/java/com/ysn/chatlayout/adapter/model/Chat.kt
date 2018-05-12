4205c720b119a6fbe134ab490137d945b0e5ca45
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-12 21:38:20 +0700

    Create class model Chat.kt file

---
+++ app/src/main/java/com/ysn/chatlayout/adapter/model/Chat.kt
@@ -0,0 +1,13 @@
+/*
+ * Created by YSN Studio on 5/12/18 4:28 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/12/18 4:28 PM
+ */
+
+package com.ysn.chatlayout.adapter.model
+
+data class Chat(
+        val message: String,
+        val dateTime: String
+)
