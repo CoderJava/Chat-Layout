bfaedb96d3fbe4b8bed367833940ebdcce082834
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-12 21:39:23 +0700

    Set object AdapterChat to RecyclerView in MainActivity.kt file

--- app/src/main/java/com/ysn/chatlayout/MainActivity.kt
+++ app/src/main/java/com/ysn/chatlayout/MainActivity.kt
@@ -9,11 +9,29 @@ package com.ysn.chatlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.ysn.chatlayout.adapter.AdapterChat
+import com.ysn.chatlayout.adapter.model.Chat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listViewType = mutableListOf<Int>()
+        listViewType.add(1)
+        listViewType.add(2)
+        listViewType.add(1)
+        listViewType.add(2)
+        val listChat = mutableListOf<Chat>()
+        listChat.add(Chat(message = "Hello", dateTime = "12-05-2018 16:36"))
+        listChat.add(Chat(message = "Hi", dateTime = "12-05-2018 16:40"))
+        listChat.add(Chat(message = "How are you?", dateTime = "12-05-2018 16:41"))
+        listChat.add(Chat(message = "I'm fine, Thanks. You?", dateTime = "12-05-2018 16:42"))
+        val adapterChat = AdapterChat(listViewType = listViewType, listChat = listChat)
+        recycler_view_chat_activity_main.layoutManager = LinearLayoutManager(this)
+        recycler_view_chat_activity_main.adapter = adapterChat
     }
 }
